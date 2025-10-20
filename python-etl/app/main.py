b272dff8125bb80be2d1a3ae0e94ed5670355988
Karim BOUALAM <boualam.karim94@gmail.com>
2025-10-20 02:13:18 +0200

    add Dockerfil

--- python-etl/app/main.py
+++ python-etl/app/main.py
@@ -1,8 +1,24 @@
 from fastapi import FastAPI
-import requests
+import psycopg2
+import os
 
 app = FastAPI()
 
 @app.get("/")
 def root():
     return {"message": "Hello from FastAPI Data Worker!"}
+
+@app.get("/test-db")
+def test_db():
+    try:
+        conn = psycopg2.connect(
+            host=os.getenv("DB_HOST"),
+            port=os.getenv("DB_PORT"),
+            user=os.getenv("DB_USER"),
+            password=os.getenv("DB_PASSWORD"),
+            database=os.getenv("DB_NAME"),
+        )
+        conn.close()
+        return {"status": "✅ Connected to PostgreSQL Render!"}
+    except Exception as e:
+        return {"status": "❌ Connection failed", "error": str(e)}
